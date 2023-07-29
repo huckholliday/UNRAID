daddb6c27182bde5dc8c8994700ea917fb43e3b8
Logan Holliday <5070352+huckholliday@users.noreply.github.com>
2023-07-29 11:43:07 -0700

    Update influxdb-backup.sh

--- influxdb-backup.sh
+++ influxdb-backup.sh
@@ -14,9 +14,6 @@ contrainerid=$(docker ps -aqf "name=Influxdb-1.8") # Update name field if your c
 dest=/backups/$day # This is the path mounted in your InfluxDB docker container
 dest2="/mnt/user/Backups/Influx" # Local path on your UNRAID server
 
-# Find and remove backups/files older than 14 days.
-find $dest2 -mtime +14 -exec rm {} \;
-
 # Backup the files using Influx command.
 docker exec -i $contrainerid influxd backup -portable $dest
 
@@ -25,6 +22,9 @@ tar czf $dest2/$archive_file $dest2/$day
 
 # Clean up temp backup folder
 rm -rf $dest2/$day
+
+# Find and remove backups/files older than 14 days.
+find $dest2 -mtime +14 -exec rm {} \;
 
 
 # Send Discord alert
