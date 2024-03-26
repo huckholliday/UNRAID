8cc091599506cd5a09df132d4d3c932d057230bd
Logan Holliday <5070352+huckholliday@users.noreply.github.com>
2024-03-26 16:55:40 -0700

    Cleaning up appdata backup script logging

--- appdata-backup.sh
+++ appdata-backup.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 ####################################
 #
-# Backup up and compression of appdata.
+# Backup up and compression of appdata folder on UNRAID.
 # You will need the Discord.sh script saved on your UNRAID server https://github.com/fieu/discord.sh
 #
 ####################################
@@ -8,55 +9,35 @@
 # Varibles sprinkled in the script
 day=$(date +%F)
 hostname=$(hostname -s)
-archive_file="$hostname-appdata-$day.tgz"
-
-# Folder locations
-dest="/mnt/user/Backups/Docker/$day"
-dest2="/mnt/user/Backups/Docker"
-
-# Clean up messsage.
-echo "Cleaning up 14+ day old backups from $dest2"
-date
-echo
+archive_file="$hostname-appdata-$day.tgz" #What you want the file to be named
+dest="/mnt/user/Backups/Docker/$day" #First save location for rsync
+dest2="/mnt/user/Backups/Docker" #Main location used for tar compression of rsync folder
 
 # Find and remove backups/files older than 30 days.
-#find $dest2 -mtime +14 -exec rm {} \;
+echo "Cleaning up 30+ day old backups from $dest2"
+find $dest2 -mtime +30 -exec rm {} \;
 
-rsync -auhv --exclude 'Plex-Media-Server' /mnt/user/appdata/* $dest
-echo "appdata folder backup excluding Plex-Media-Server complete"
-echo
-date
+#Copy appdata folder and skip Plex so it does not hold up other container folders
+echo "Starting backup of appdata folder"
+rsync -auh --exclude 'Plex-Media-Server' /mnt/user/appdata/* $dest
+echo "Backup of appdata folder complete"
 sleep 3
 
-rsync -auhv --exclude 'Library/Application Support/Plex Media Server/Cache' /mnt/user/appdata/Plex-Media-Server $dest
-echo "Plex-Media-Server backup complete"
-echo
-date
+# Copy Plex folder but exclude cache since that is data can be rebuilt after a restore
+echo "Starting backup of Plex-Media-Server"
+rsync -auh --exclude 'Library/Application Support/Plex Media Server/Cache' /mnt/user/appdata/Plex-Media-Server $dest
+echo "Backup of Plex-Media-Server complete"
 sleep 3
-
-echo
-echo "tar balling appdata folder"
-date
-echo
 
 # Compress backed up data
-tar cvzf $dest2/$archive_file $dest2/$day
-
-# Print clean up status message.
-echo
-echo "Removing temp folder $dest2/$day"
-date
-echo
+echo "Starting tar balling appdata folder"
+tar czf $dest2/$archive_file $dest2/$day
+echo "Tar balling of appdata folder complete"
 
 # Clean up temp folder
+echo "Removing temp folder $dest2/$day"
 rm -rf $dest2/$day
 
-# Print end status message.
-echo
-echo "Backup completed of appdata"
-date
-date
-
 # Send Discord alert
-# https://github.com/fieu/discord.sh
+echo "Backup completed of appdata"
 bash /mnt/user/Backups/Scripts/discord.sh --webhook-url="ADD-DISCORD-WEBHOOK-URL-HERE" --username "APPDATA Backup" --title "$HOSTNAME APPDATA Backup Complete" --description "The backup of appdata on $HOSTNAME has completed." --timestamp
